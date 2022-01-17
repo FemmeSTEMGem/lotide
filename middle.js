cc30a83a9921b2e814fd8e93ca2a792e24af1196
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-17 17:14:41 -0500

    refactored for testing

--- middle.js
+++ middle.js
@@ -1,22 +1,4 @@
-const assertArraysEqual = function(array1, array2, assertArg) {
-  if (eqArrays(array1, array2) === assertArg) {
-    console.log(`😻😻😻 Assertion Passed: ${array1} and ${array2} === ${assertArg}`);
-  } else {
-    console.log(`😿😿😿 Assertion Failed: ${array1} and ${array2} !== ${assertArg}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   if (array.length === 1 || array.length === 2) {
@@ -34,13 +16,4 @@ const middle = function(array) {
   }
 }
 
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-
-
-// 1, 2, 3, 4, 5, 6, 7, 8 (4 & 5 are the middle two numbers)
-//array.length will = 8
-//position of middle two numbers is 4 & 5
-
-// 1, 2, 3, 4, 5, 6, 7, 8, 9 (5 is the middle number)
-//array.length will = 9
-//position of middle number is 5
+module.exports = middle;
