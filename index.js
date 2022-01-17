0733e523f00d0288fe522d6ccf5601d1dbc08ebb
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-17 18:27:07 -0500

    first commit, refactored with ES6 syntax

---
+++ index.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual       = require('./assertEqual');
+const eqArrays          = require('./eqArrays');
+const head              = require('./head');
+const tail              = require('./tail');
+const middle            = require('./middle');
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  head,
+  tail,
+  middle,
+};
