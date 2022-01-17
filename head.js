08ef1f1a834e9f2be67122a40501a29e39c8becd
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-17 13:26:19 -0500

    refactoring for export

--- head.js
+++ head.js
@@ -1,11 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`😿😿😿 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`😻😻😻 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 function head(array) {
-  return array[0];
+  return array[0]
 }
+
+
+assertEqual(head([5,6,7]), 5);
